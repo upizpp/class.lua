c97a517228505253cead6d9207795cf21cb9d51f
upizpp <82654854+upizpp@users.noreply.github.com>
2023-04-22 20:56:07 +0800

    Add files via upload

---
+++ LuaExtensions.lua
@@ -0,0 +1,265 @@
+-- StringExtensions.lua:
+---分割字符串。
+---@param delimiter string 割符，<strong>它既可以是字符串，也可以是正则表达式。</strong>
+---@param allow_empty boolean? # 如果为true，结果中就会包含空字符串。
+---@param maxsplit integer? # 就是最多分割的结果个数。
+---@param remove boolean? # 如果为true，当超过maxsplit时会将多余的部分删除。
+---@return table
+function string:split(delimiter, allow_empty, maxsplit, remove)
+	allow_empty = allow_empty or false
+	maxsplit = maxsplit or -1
+	if remove == nil then remove = true end
+	local lto = 0
+	local from, to = self:find(delimiter)
+	local result = {}
+	local idx = 0
+	local function insert(s)
+		if not allow_empty then
+			if s ~= "" then
+				table.insert(result, s)
+			end
+		else
+			table.insert(result, s)
+		end
+	end
+	while from ~= nil do
+		if maxsplit ~= -1 then
+			if not remove then
+				if idx >= maxsplit - 1 then
+					insert(self:sub(lto + 1))
+					return result
+				end
+			elseif idx >= maxsplit then
+				return result
+			end
+		end
+		idx = idx + 1
+		insert(self:sub(lto + 1, from - 1))
+		lto = to
+		from, to = self:find(delimiter, to + 1)
+	end
+	if maxsplit ~= -1 and remove and idx >= maxsplit then
+		return result
+	end
+	insert(self:sub(lto + 1))
+	return result
+end
+
+function string.byte_length(b)
+	local d
+	if b > 239 then
+		d = 4
+	elseif b > 223 then
+		d = 3
+	elseif b > 128 then
+		d = 2
+	else
+		d = 1
+	end
+	return d
+end
+
+function string:to_byte_table()
+	local result = {}
+	local i = 1
+	while true do
+		local b = self:sub(i, i):byte()
+		local d = string.byte_length(b)
+		local s = self:sub(i, i + d - 1)
+		table.insert(result, b)
+		i = i + d
+		if (i > self:len()) then
+			break
+		end
+	end
+	return result
+end
+
+---若此字符串以text开头，返回true。
+---@param text string
+---@return boolean
+function string:begins_with(text)
+	return self:sub(0, text:len()) == text
+end
+
+---若此字符串以text结尾，返回true。
+---@param text string
+---@return boolean
+function string:ends_with(text)
+	return self:sub(self:len() - text:len() + 1, self:len()) == text
+end
+
+---返回字符串是否为空。
+---@return boolean
+function string:empty()
+	return self == ""
+end
+
+---向后添加文件path
+---@param path string
+---@return string
+function string:plus_file(path)
+	if self:ends_with("/") then
+		return self .. path
+	elseif self:empty() then
+		return path
+	else
+		return self .. "/" .. path
+	end
+end
+
+---如果字符串是一个有效的文件名或路径，返回不带句点的扩展名（.）。如果字符串不包含扩展名，则返回nil。
+---@return string|nil
+function string:get_extension()
+	for i = self:len(), 1, -1 do
+		if self:sub(i, i) == "." then
+			return self:sub(i + 1)
+		end
+	end
+	return nil
+end
+
+---字符串为有效文件路径时，返回基础目录名。
+---@return string|stringlib
+function string:get_base_dir()
+	for i = self:len(), 1, -1 do
+		if self:sub(i, i) == "/" then
+			return self:sub(1, i - 1)
+		end
+	end
+	return self
+end
+
+---字符串为有效文件路径时，返回完整的文件路径，不带扩展名。
+---@return string|stringlib
+function string:get_basename()
+	for i = self:len(), 1, -1 do
+		if self:sub(i, i) == "." then
+			return self:sub(1, i - 1)
+		end
+	end
+	return self
+end
+
+---字符串为有效文件路径时，返回文件名。 
+---@return string|stringlib
+function string:get_file()
+	for i = self:len(), 1, -1 do
+		if self:sub(i, i) == "/" then
+			return self:sub(i + 1)
+		end
+	end
+	return self
+end
+
+
+function table:join(delimiter)
+	local result = ""
+	for i = 1, #self do
+		result = result .. self[i] .. delimiter
+	end
+	return result:sub(1, result:len() - delimiter:len())
+end
+
+---复制一个表。
+---@return table
+function table.copy(tab)
+	if tab == nil then
+		return {}
+	end
+	local result = {}
+	for key, value in pairs(tab) do
+		if type(value) == "table" then
+			result[key] = table.copy(value)
+		else
+			result[key] = value
+		end
+	end
+	return result
+end
+
+---@param what any
+---@return boolean
+function table:has(what)
+	for key, value in pairs(self) do
+		if value == what then
+			return true
+		end
+	end
+	return false
+end
+
+---@param what any
+---@return any
+function table:find(what)
+	for key, value in pairs(self) do
+		if value == what then
+			return key
+		end
+	end
+	return nil
+end
+
+---@param a table
+---@param b table
+---@param overwrite boolean?
+---@return table
+function table.merge(a, b, overwrite)
+	overwrite = overwrite or false
+	a = a or {}
+	b = b or {}
+	local result = table.copy(a)
+	local offset = 0
+	for key, value in pairs(b) do
+		if type(key) == "number" then
+			while not overwrite and a[key + offset] ~= nil do
+				offset = offset + 1
+			end
+			result[key + offset] = value
+		else
+			if overwrite then
+				if type(value) == "table" then
+					result[key] = table.merge(result[key], value, overwrite) 
+				else
+					result[key] = value
+				end
+			elseif a[key] == nil then
+				if type(value) == "table" then
+				   result[key] = table.merge(result[key], value, overwrite) 
+				else
+					result[key] = value
+				end
+			end
+		end
+	end
+	return result
+end
+
+---新建一个枚举
+---@param table table
+---@param step number?
+---@return table
+function table.enum(table, step, mode)
+	step = step or 1
+	mode = mode or 0
+	local result = {}
+	local current
+	if mode == 0 then
+		current = 0
+	elseif mode == 1 then
+		current = 1
+	end
+	for i = 1, #table do
+		if type(table[i]) == "number" then
+			current = table[i]
+		end
+		result[table[i]] = current
+		if mode == 0 then
+			current = current + step
+		elseif mode == 1 then
+			current = current * step
+		end
+	end
+	return result
+end
+
---
+++ class.lua
@@ -0,0 +1,211 @@
+class = {}
+class.ObjectPool = {}
+class.datas = {}
+
+
+require "LuaExtensions"
+
+
+local function safe_setmetatable(table, meta)
+	if table ~= nil and meta ~= nil then
+		setmetatable(table, meta)
+	end
+end
+
+
+function class:new(arguments)
+	local result = arguments
+
+	local class_name = arguments[1]
+	local extends_from = arguments[2]
+
+	local succeed, full_path = self:get_full_path(extends_from)
+	if succeed then
+		setmetatable(result, {
+			__index = class.datas[full_path]
+		})
+		safe_setmetatable(result.__setters__, {
+			__index = class.datas[full_path].__setters__
+		})
+		safe_setmetatable(result.__getters__, {
+			__index = class.datas[full_path].__getters__
+		})
+	end
+
+	result[1] = nil
+	result[2] = nil
+	result.class_name = class_name
+	if result.__constructor__ == nil then
+		result.__constructor__ = function() end
+	end
+
+	function result:new(...)
+		return class:instance(class_name, ...)
+	end
+
+	self.datas[full_path:plus_file(class_name)] = result
+	return result
+end
+
+function class:rawget(object, key)
+	local result = rawget(object, key)
+	if result == nil then
+		return rawget(object, "__data")[key]
+	end
+	return result
+end
+
+function class:manipulate_variable(object, key)
+	local value = self:rawget(object, key)
+	if self:is_placeholder(value) then
+		local member_object = self:instance(value.__class_name__, table.unpack(value.__arguments__))
+		rawset(member_object, "__ref", {
+			["object"] = object,
+			["key"] = key
+		})
+		rawset(object, key, member_object)
+	end
+end
+
+function class:instance(class_name, ...)
+	local succeed, full_path = self:get_full_path(class_name)
+	if not succeed then
+		return nil
+	end
+	
+	local data = self.datas[full_path]
+
+	local result = {}
+
+	result.__data = data
+	result.__locked = false
+	result.__lock = function() rawset(result, "__locked", true) end
+	result.__unlock = function() rawset(result, "__locked", false) end
+
+	result.class_name = class_name
+	result.full_class_name = full_path
+
+	local function super(self)
+		local paths = self.full_class_name:split("/")
+		table.remove(paths, #paths)
+		if #paths == 0 then
+			error("Try to call 'super', but the class has no parent class.")
+		end
+		local full_class_name = table.join(paths, "/")
+		local data = class.datas[full_class_name]
+		local parent = {}
+		parent.full_class_name = full_class_name
+		parent.super = super
+		setmetatable(parent, {
+			__index = function(table, key)
+				if type(data[key]) == "function" then
+					return data[key]
+				elseif key == "super" then
+					return super
+				end
+			end
+		})
+		return parent
+	end
+	result.super = super
+	
+	local metas = {
+		__index = function(table, key)
+			self:manipulate_variable(table, key)
+			if rawget(table, "__locked") then
+				return self:rawget(table, key)
+			end
+			rawget(table, "__lock")()
+			if table.__getters__ ~= nil then
+				if table.__getters__[key] ~= nil then
+					local value = table.__getters__[key](table)
+					table.__unlock()
+					return value
+				elseif table.__getters__.__any_other__ ~= nil then
+					return table.__getters__.__any_other__(table, key)
+				end
+			end
+			table.__unlock()
+			return self:rawget(table, key)
+		end,
+		__newindex = function(table, key, value)
+			self:manipulate_variable(table, key)
+			data[key] = value
+			if rawget(table, "__locked") then
+				rawset(table, key, value)
+				return
+			end
+			rawget(table, "__lock")()
+			if table.__ref and table.__ref.object.__setters__ then
+				rawget(table.__ref.object, "__lock")()
+				if table.__ref.object.__setters__.__any__ ~= nil then
+					table.__ref.object.__setters__.__any__(table, table.__ref.key, table)
+				end
+				if table.__ref.object.__setters__[table.__ref.key] ~= nil then
+					table.__ref.object.__setters__[table.__ref.key](table, table)
+				else
+					if table.__ref.object.__setters__.__any_other__ ~= nil then
+						table.__ref.object.__setters__.__any_other__(table, table.__ref.key, table)
+					end
+				end
+				table.__ref.object.__unlock()
+			end
+			if table.__setters__ ~= nil then
+				if table.__setters__.__any__ ~= nil then
+					table.__setters__.__any__(table, key, value)
+				end
+				if table.__setters__[key] ~= nil then
+					table.__setters__[key](table, value)
+					table.__unlock()
+					return
+				else
+					if table.__setters__.__any_other__ ~= nil then
+						table.__setters__.__any_other__(table, key, value)
+					end
+				end
+			end
+			table.__unlock()
+			rawset(table, key, value)
+		end,
+		__pairs = function (table)
+			return pairs(data)
+		end
+	}
+	if data.__metas__ then
+		metas = table.merge(metas, data.__metas__, false)
+	end
+	setmetatable(result, metas)
+	
+	result:__constructor__(...)
+
+	return result
+end
+
+function class:make_placeholder(class_name, ...)
+	return {
+		__is_placeholder__ = true,
+		__class_name__ = class_name,
+		__arguments__ = {...}
+	}
+end
+
+function class:is_placeholder(value)
+	return type(value) == "table" and value.__is_placeholder__ == true and value.__class_name__ and value.__arguments__
+end
+
+function class:get_full_path(class_name)
+	if class_name == nil then
+		return false, ""
+	end
+	for k, v in pairs(class.datas) do
+		local paths = k:split("/")
+		if paths[#paths] == class_name then
+			return true, k
+		end
+	end
+	return false, class_name
+end
+
+setmetatable(class, {
+	__call = class.new
+})
